57eebd81f91a475ac90b13305bbe84bcccf32055
bigshowxin <bigshowxin@163.com>
2015-10-29 21:10:16 +0800

    rss and paginator

--- article/models.py
+++ article/models.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
 class Article(models.Model):
+
     title = models.CharField(max_length=100)                #博客标题
     category = models.CharField(max_length=50,blank=True)   #博客标签
     date_time = models.DateTimeField(auto_now_add=True)     #博客日期
@@ -12,5 +14,10 @@ class Article(models.Model):
     def __unicode__(self):
         return self.title
 
-class Meta:
-    ordering=['-date_time']
+    def get_absolute_url_2(self):
+        path = reverse('detail',kwargs={'id':self.id})
+        return "127.0.0.1:8001%s" %path
+
+
+    class Meta:
+        ordering=['-date_time']
